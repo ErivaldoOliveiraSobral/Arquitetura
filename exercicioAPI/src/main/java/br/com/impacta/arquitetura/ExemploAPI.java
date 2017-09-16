b370e73974b74a9a2380b727378a5ace5c6358d0
Erivaldo Oliveira Sobral <erivaldooliveirasobral@hotmail.com>
2017-09-16 09:04:30 -0300

    Exercicio de arquitetura de software

---
+++ exercicioAPI/src/main/java/br/com/impacta/arquitetura/ExemploAPI.java
@@ -0,0 +1,13 @@
+package br.com.impacta.arquitetura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploAPI {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploAPI.class, args);
+	}
+
+}
